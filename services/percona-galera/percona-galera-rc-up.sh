cd6a178739dab870aa205bf10521f51b2404c707
Tim Smart <tim.smart@arisechurch.com>
2015-05-07 23:04:15 +1200

    MySQL password generation
    Signed-off-by: Tim Smart <tim.smart@arisechurch.com>

--- services/percona-galera/percona-galera-rc-up.sh
+++ services/percona-galera/percona-galera-rc-up.sh
@@ -2,7 +2,9 @@
 
 script_root=`dirname "${BASH_SOURCE}"`
 discovery=$(curl -sL https://discovery.etcd.io/new)
+password=$(uuidgen)
 
 cat $script_root/percona-galera-rc-template.yaml | \
     sed -e "s,{{discovery}},$discovery,g" | \
+    sed -e "s,{{password}},$password,g" | \
     kubectl create -f -
