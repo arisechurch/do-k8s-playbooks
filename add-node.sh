d05f7ddf9d8e1ce25febc8c652b1432111ee331e
Tim Smart <tim.smart@arisechurch.com>
2015-05-08 12:28:03 +1200

    Add private docker repos
    Signed-off-by: Tim Smart <tim.smart@arisechurch.com>

--- add-node.sh
+++ add-node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-playbook -i inventories/digitalocean.sh create-node.yaml
-ansible-playbook -i inventories/digitalocean.sh update-iptables.yaml
+ansible-playbook -i inventories/digitalocean.sh prepare-cluster.yaml
